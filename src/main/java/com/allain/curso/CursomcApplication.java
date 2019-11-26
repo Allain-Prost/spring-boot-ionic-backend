2063d4ef04fbfc9333e758ba40741decf9ac0d1b
Allain Prost <allain.prost@dcx.ufpb.br>
2019-11-25 23:06:13 -0300

    Criando estados e cidades

--- src/main/java/com/allain/curso/CursomcApplication.java
+++ src/main/java/com/allain/curso/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.allain.curso.domain.Categoria;
+import com.allain.curso.domain.Cidade;
+import com.allain.curso.domain.Estado;
 import com.allain.curso.domain.Produto;
 import com.allain.curso.repositories.CategoriaRepository;
+import com.allain.curso.repositories.CidadeRepository;
+import com.allain.curso.repositories.EstadoRepository;
 import com.allain.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -40,9 +48,22 @@ public class CursomcApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		Estado estd1 = new Estado(null,"Minas Gerais");
+		Estado estd2 = new Estado(null,"São Paulo");
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Cidade c1 = new Cidade(null, "Uberlâdia", estd1);
+		Cidade c2 = new Cidade(null, "São Paulo", estd2);
+		Cidade c3 = new Cidade(null, "Campinas", estd2);
+		
+		estd1.getCidades().addAll(Arrays.asList(c1));
+		estd2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(estd1,estd2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 
 }
---
+++ src/main/java/com/allain/curso/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.allain.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.allain.curso.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
+
